8d8a264d74b05f6afc414300d2ca312c5c9310b9
Fan002 <421914774@qq.com>
2023-02-20 17:57:48 +0800

    leetcode

--- Medium/src/LongestPalindromicSubstring.java
+++ Medium/src/LongestPalindromicSubstring.java
@@ -4,6 +4,7 @@
 public class LongestPalindromicSubstring {
     public static void main(String[] args) {
         String s = "babab";
+        System.out.println(s);
     }
 
     public static String longestPalindrome(String s) {
